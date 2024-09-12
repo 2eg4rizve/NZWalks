613d701b41f2140cd87b6612e03a39e0ebc34e50
richr <richr@RIZVE>
2024-09-12 16:24:55 +0600

    Add project files.

---
+++ NZWalks.API/Migrations/20240909062944_debugDb.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NZWalks.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class debugDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RegionImageUrl",
+                table: "Regions",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RegionImageUrl",
+                table: "Regions");
+        }
+    }
+}
---
+++ NZWalks.API/Models/Domain/JsonPropertyAttribute.cs
@@ -0,0 +1,7 @@
+﻿
+namespace NZWalks.API.Models.Domain
+{
+    internal class JsonPropertyAttribute : Attribute
+    {
+    }
+}
