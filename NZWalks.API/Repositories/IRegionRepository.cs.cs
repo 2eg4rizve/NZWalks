8853b0bb035844555cd4628fa60c569e9425ac2b
2eg4rizve <2eg4rizve@gmail.com>
2024-09-15 09:46:08 +0600

    section 7

--- NZWalks.API/Repositories/IRegionRepository.cs.cs
+++ NZWalks.API/Repositories/IRegionRepository.cs.cs
@@ -4,7 +4,7 @@ namespace NZWalks.API.Repositories
 {
     public interface IRegionRepository
     {
-        //go to sql repository
+        // //go to sql repository
         Task<List<Region>>GetAllAsync();
 
         Task<Region?> GetByIdAsync(Guid id);
